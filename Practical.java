3df46a90b68b87e9344c854c233744854cd44f04
Pratik <pratik216029@gmail.com>
2026-08-19 01:48:46 +0530

    DONE

--- Practical.java
+++ Practical.java
@@ -10,6 +10,7 @@ class Practical{
         } 
         System.out.println("Cipher Text: "+cipherText.toString());
         return cipherText.toString();
+        
     }     
     static void decrypt(String plainText,String key){
         StringBuilder plain=new StringBuilder();
